6054923f94e67ee3d368256400411b4362233d7e
卞留念 <554565808@qq.com>
2016-06-29 23:37:39 +0800

    添加模态框

--- src/js/admin/set_material_kind.js
+++ src/js/admin/set_material_kind.js
@@ -2,9 +2,23 @@
  * Created by bln on 16-6-29.
  */
 require('../common/shared.js');
+
 require('../../bower_components/toastr/toastr.min.css');
 require('../../scss/admin/set_material_kind.scss');
 
 require('../../bower_components/toastr/toastr.min.js');
+require('angular');
+
 var $ = jQuery;
-require('imports?$=jquery!../../bower_components/angular/angular.min');
+
+$(function () {
+    var app = angular.module('app',[]);
+
+    app.controller('MainCtrl',['$scope',function(scope){
+        scope.createFile = function(){
+              
+        };
+    }]);
+
+    angular.bootstrap(document.documentElement, ['app']);
+});
