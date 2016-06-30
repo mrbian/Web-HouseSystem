4a63df4a4987a443fef4c42c107fa743866fdb19
卞留念 <554565808@qq.com>
2016-06-30 15:51:50 +0800

    模态框样式完成

--- src/js/admin/set_material_kind.js
+++ src/js/admin/set_material_kind.js
@@ -8,16 +8,28 @@ require('../../scss/admin/set_material_kind.scss');
 
 require('../../bower_components/toastr/toastr.min.js');
 require('angular');
+var WebUploader = require('../../bower_components/fex-webuploader/dist/webuploader.min.js');
+
+var uploader = WebUploader.create({
+    server : '',
+    pick : '#picker',
+    resize : false
+});
 
 var $ = jQuery;
 
 $(function () {
+
     var app = angular.module('app',[]);
 
     app.controller('MainCtrl',['$scope',function(scope){
         scope.createFile = function(){
-              
+
         };
+    }]);
+
+    app.controller('ModalCtrl',['$scope','$http',function(scope,$http){
+
     }]);
 
     angular.bootstrap(document.documentElement, ['app']);
