be78359fa42bde73618e3e8bd83e955b4211f7da
lirz <lirunzhi99@hotmail.com>
2016-06-30 21:20:29 +0800

    fix style

--- src/js/user/form.js
+++ src/js/user/form.js
@@ -4,6 +4,7 @@
 require('../common/shared');
 require('../../scss/user/form.scss');
 require('expose?Webuploader!../../bower_components/fex-webuploader/dist/webuploader');
+require('angular-toastr');
 var $ = jQuery;
 
 // var Webuploader = Webuploader;
@@ -13,66 +14,78 @@ $(document).ready(function () {
     var app = angular.module('app', []);
     var progress = $('#progress');
     var temp = '';
-    var uploader = Webuploader.create({
-        // 不压缩image
-        resize: false,
-        chunked: false,
-        fileNumLimit: 1,
-        // swf文件路径
-        swf: '../bower_components/fex-webuploader/dist/Uploader.swf',
-        dnd: '#container',
-        // 文件接收服务端。
-        server: '/user/form',
-        auto: false,
-        // 选择文件的按钮。可选。
-        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-        pick: '#picker'
-        // accept: {
-        //     title: 'Images',
-        //     extensions: 'gif,jpg,jpeg,bmp,png',
-        //     mimeTypes: 'image/*'
-        // }
-    });
-
-    // 当有文件添加进来的时候
-    uploader.on('fileQueued', function (file) {
-        console.log(file);
-        temp = file.id;
-    });
-
-    // 文件上传过程中创建进度条实时显示。
-    uploader.on('uploadProgress', function (file, percentage) {
-        // var $li = $('#' + file.id),
-        var $percent = progress.find('.progress .progress-bar');
-
-        // 避免重复创建
-        if (!$percent.length) {
-            $percent = $('<div class="row"><div class="col-md-1"></div>'
-                + '<div class="col-md-9"><div class="progress progress-striped active">'
-                + '<div class="progress-bar" role="progressbar" style="width:0;">'
-                + '</div>' + '</div></div></div>').appendTo(progress);//.find('.progress-bar');
-        }
-
-        // // $li.find('#' + file.id).text('上传中');
-        //
-        $percent.css('width', percentage * 100 + '%');
-    });
-
-    uploader.on('uploadSuccess', function (file) {
-        progress.text('<p>上传成功</p>')
-    });
-
-    uploader.on('uploadError', function (file) {
-        progress.text('<p>上传失败</p>')
-    });
-
-    uploader.on('uploadComplete', function (file) {
-       progress.html();
-    });
-
-    uploader.on('uploadAccept', function (object, ret) {
-
-    });
+    var abort;
+    // var uploader = Webuploader.create({
+    //     // 不压缩image
+    //     resize: false,
+    //     chunked: false,
+    //     fileNumLimit: 1,
+    //     // swf文件路径
+    //     swf: '../bower_components/fex-webuploader/dist/Uploader.swf',
+    //     dnd: '#container',
+    //     // 文件接收服务端。
+    //     server: '/user/form',
+    //     auto: false,
+    //     // 选择文件的按钮。可选。
+    //     // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+    //     pick: '#picker'
+    //     // accept: {
+    //     //     title: 'Images',
+    //     //     extensions: 'gif,jpg,jpeg,bmp,png',
+    //     //     mimeTypes: 'image/*'
+    //     // }
+    // });
+    //
+    // // 当有文件添加进来的时候
+    // uploader.on('fileQueued', function (file) {
+    //     console.log(file);
+    //     temp = file.id;
+    //     progress.append('<p>文件准备上传</p>' +
+    //         '<button class="btn btn-danger" id="abort">取消</button>');
+    //     $('#container').hide();
+    //     abort = $('#abort');
+    //     abort.on('click', function () {
+    //         uploader.reset();
+    //         $('#container').show();
+    //         progress.empty();
+    //     });
+    // });
+    //
+    // // 文件上传过程中创建进度条实时显示。
+    // uploader.on('uploadProgress', function (file, percentage) {
+    //     // var $li = $('#' + file.id),
+    //     var $percent = progress.find('.progress .progress-bar');
+    //
+    //     // 避免重复创建
+    //     if (!$percent.length) {
+    //         $percent = $('<div class="row"><div class="col-md-1"></div>'
+    //             + '<div class="col-md-9"><div class="progress progress-striped active">'
+    //             + '<div class="progress-bar" role="progressbar" style="width:0;">'
+    //             + '</div>' + '</div></div></div>').appendTo(progress);//.find('.progress-bar');
+    //     }
+    //
+    //     // // $li.find('#' + file.id).text('上传中');
+    //     //
+    //     $percent.css('width', percentage * 100 + '%');
+    // });
+    //
+    // uploader.on('uploadSuccess', function (file) {
+    //     progress.empty();
+    //     progress.append('<p>上传成功</p>')
+    // });
+    //
+    // uploader.on('uploadError', function (file) {
+    //     progress.empty();
+    //     progress.append('<p>上传失败</p>')
+    // });
+    //
+    // uploader.on('uploadComplete', function (file) {
+    //     progress.empty();
+    // });
+    //
+    // uploader.on('uploadAccept', function (object, ret) {
+    //
+    // });
     
 
     app.controller('formController', ['$scope', '$http', function (scope, http) {
@@ -115,6 +128,11 @@ $(document).ready(function () {
             '集资建房'
         ];
 
+        scope.file_items = [{
+            name: '日出江花',
+            if_need: false
+        }];
+        
         
         scope.changePowerButton = function (type, text) {
 
@@ -145,6 +163,9 @@ $(document).ready(function () {
                     break;
             }
         };
+        
+        // http.post()
+
     }]);
 
     angular.bootstrap(document, ['app']);
